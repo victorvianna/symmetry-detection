45d026edeff64c42aac5b18718f4cd3c82664e35
victorvianna <victor.vianna10@gmail.com>
2018-12-09 19:10:42 +0100

    Add nearest neighbors lib
    Closes #2

--- test/main.cpp
+++ test/main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include "mean_shift.h"
 #include "kernel.h"
+#include "flann/flann.hpp"
 
 void test_mesh_display(){
   Eigen::MatrixXd V;
@@ -42,9 +43,41 @@ void test_clustering(){
     vector<Cluster> clusters = msp->cluster(points, kernel_bandwidth);
 }
 
+void test_nearest_neighbors(){
+    using namespace flann;
+
+    double* dataset_ = new double[8]{
+            0, 0,
+            1, 1,
+            2, 2,
+            3, 3
+    };
+    Matrix<double> dataset(dataset_, 4, 2);
+    double* query_ = new double[2]{
+            0.5, 0.5
+    };
+    Matrix<double> query(query_, 1, 2);
+
+    std::vector< std::vector<int>> indices;
+    std::vector<std::vector<double>> dists;
+
+    // construct an randomized kd-tree index using 4 kd-trees
+
+    Index<L2<double>> index(dataset, flann::KDTreeIndexParams(4));
+    index.buildIndex();
+
+    // search around a certain radius
+    double radius = 0.6;
+    index.radiusSearch(query, indices, dists, radius, flann::SearchParams(128));
+
+    delete[] dataset.ptr();
+    delete[] query.ptr();
+}
+
 int main(int argc, char *argv[])
 {
   //test_mesh_display();
   test_clustering();
+  test_nearest_neighbors();
   return 0;
 }
